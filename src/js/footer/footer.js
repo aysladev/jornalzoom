3211e370aae79af24422fb1e494ed898db155e66
Aysla <aysladev@gmail.com>
2025-10-02 22:42:00 -0300

    feat. add footer

---
+++ src/js/footer/footer.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html").then(response => response.text()).then(data => {
+        document.body.insertAdjacentHTML("beforeend", data);
+    }).catch(error => console.error("Erro ao carregar footer:", error));
+});
